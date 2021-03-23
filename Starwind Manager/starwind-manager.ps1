f5e4c9d36a826646ead99ff824cbad29b2d758af
Omar Ait-Moulid <omar.ait@hotmail.fr>
2021-03-23 18:30:53 +0100

    update

--- Starwind Manager/starwind-manager.ps1
+++ Starwind Manager/starwind-manager.ps1
@@ -25,42 +25,43 @@ $port = ""
 # Utilisation : se lance au démarrage du script, fait office de menu principal
 # =======================================================
 
-Function startprogram
-{
-    do 
-    {
+Function startprogram {
+	do {
 		# Affichage informatif sur le serveur en cours d'utilisation
-	    if ($global:server) { Write-host "Connected to Server $global:srv on port $global:port" -foreground green }
-	    Write-host ""
+		if ($global:server) { Write-host "Connected to Server $global:srv on port $global:port" -foreground green }
+		Write-host ""
 		# Menu
-	    Write-host "##################################" -foreground yellow
-	    Write-host "[0] Connect to Server"
-	    Write-host "[1] Disconnect"
-	    Write-host "[2] Enumerate all Targets and Devices"
-	    Write-host "[3] Create Device"
-	    Write-host "[4] Extend Device"
-	    Write-host "[5] Remove Device"
-	    Write-host "[6] Remove Target"
-	    Write-host "[7] Disconnect and close"
-	    Write-host "##################################" -foreground yellow
-	    Write-host ""
-	    $choice = Read-host "Enter your choice" 
+		Write-host "##################################" -foreground yellow
+		Write-host "[0] Connect to Server"
+		Write-host "[1] Disconnect"
+		Write-host "[2] Enumerate all Targets and Devices"
+		Write-host "[3] Create Device"
+		Write-host "[4] Extend Device"
+		Write-host "[5] Remove Device"
+		Write-host "[6] Create Target"
+		Write-host "[7] Remove Target"
+		Write-host "[8] Detach or Attach device to target"
+		Write-host "[9] Disconnect and close"
+		Write-host "##################################" -foreground yellow
+		Write-host ""
+		$choice = Read-host "Enter your choice" 
 
 		# Appel des fonctions selon le choix
-	    switch ($choice)
-	    {
-		    0 { connect_starwind }
-		    1 { disconnect_starwind }
-		    2 { enumdevices }
-		    3 { createdevice }
-		    4 { extenddevice }
-		    5 { removedevice }
-		    6 { removetarget }
-		    7 { if ($global:server) { disconnect_starwind }; $close = 1 }
-	    }
-	    Clear-host
-    }
-    while (!$close)
+		switch ($choice) {
+			0 { connect_starwind }
+			1 { disconnect_starwind }
+			2 { enumdevices }
+			3 { createdevice }
+			4 { extenddevice }
+			5 { removedevice }
+			6 { createtarget }
+			7 { removetarget }
+			8 { detach_attach }
+			9 { if ($global:server) { disconnect_starwind }; $close = 1 }
+		}
+		Clear-host
+	}
+	while (!$close)
 }
 
 # =======================================================
@@ -68,24 +69,21 @@ Function startprogram
 # Utilisation : Demande à l'utilisateur les informations pour la connexion au serveur
 # =======================================================
 
-Function connect_starwind
-{
+Function connect_starwind {
 
-	try
-	{
+	try {
 		Write-host ""
 		# Entrée des informations de connexions
-		if (($global:srv = Read-host -Prompt "Server name or ip [default : localhost]") -eq "") {$global:srv = "localhost"}
-		if (($global:port = Read-host -Prompt "Port [default : 3261]") -eq "") {$global:port = "3261"}
-		if (($user = Read-host -Prompt "Username [default : root]") -eq "") {$user = "root"}
-		if (($password = Read-host -Prompt "Password [default : starwind]") -eq "") {$password = "starwind"}
+		if (($global:srv = Read-host -Prompt "Server name or ip [default : localhost]") -eq "") { $global:srv = "localhost" }
+		if (($global:port = Read-host -Prompt "Port [default : 3261]") -eq "") { $global:port = "3261" }
+		if (($user = Read-host -Prompt "Username [default : root]") -eq "") { $user = "root" }
+		if (($password = Read-host -Prompt "Password [default : starwind]") -eq "") { $password = "starwind" }
 
 		# Initialisation de la connexion au serveur
 		$global:server = New-SWServer $global:srv $global:port $user $password
 		$global:server.Connect()
 	}
-	catch
-	{
+	catch {
 		# En cas d'erreur, afficher le message et attendre la validation
 		Write-host $_ -foreground red
 		Read-host "Press enter to return to menu"
@@ -97,38 +95,66 @@ Function connect_starwind
 # Utilisation : Déconnexion du serveur
 # =======================================================
 
-Function disconnect_starwind
-{	
+Function disconnect_starwind {	
 	# Déconnexion du serveur
 	$global:server.Disconnect()
 	$global:server = $null
 }
+
+Function list_targets_and_devices_attached {
+	# Affichage des targets et des devices rattachés
+	$list = $global:server.Devices | Where-Object TargetId -notlike "empty" | Format-Table -AutoSize -Wrap -Property TargetId, TargetName, DeviceId, @{N = 'Device Name'; E = { $_.Name } }, @{N = 'Path'; E = { $_.File } }, @{N = 'Size MB'; E = { $_.Size / 1MB } }
+	Write-host "#################################" -foreground yellow
+	Write-host "Targets and devices attached" -foreground yellow
+	if ($list) { $list } else { Write-host "Nothing found" -foreground red }
+}
+
+Function list_targets_without_devices {
+	# Affichage des targets sans device
+	$targetsToExclude = @()
+	foreach ($device in $global:server.Devices | Where-Object TargetId -notlike "empty") {
+		$targetsToExclude += $device.TargetId
+	}
+	$list = $global:server.Targets | Where-Object Id -notin $targetsToExclude | Format-Table -AutoSize -Wrap -Property Id, Name
+	Write-host "#################################" -foreground yellow
+	Write-host "Targets wihout devices" -foreground yellow
+	if ($list) { $list } else { Write-host "Nothing found" -foreground red }
+}
+
+Function list_orphan_devices {
+	# Affichage des devices attachés à aucun target
+	$list = $global:server.Devices | Where-Object TargetId -like "empty" | Format-Table -AutoSize -Wrap  -Property DeviceId, @{N = 'Device Name'; E = { $_.Name } }, @{N = 'Path'; E = { $_.File } }, @{N = 'Size MB'; E = { $_.Size / 1MB } }
+	Write-host "#################################" -foreground yellow
+	Write-host "Devices not attached to a target" -foreground yellow
+	if ($list) { $list } else { Write-host "Nothing found" -foreground red }
+}
+
+Function list_all_devices {
+	# Affichage des devices
+	$list = $global:server.Devices | Format-Table -AutoSize -Wrap  -Property DeviceId, @{N = 'Device Name'; E = { $_.Name } }, @{N = 'Path'; E = { $_.File } }, @{N = 'Size MB'; E = { $_.Size / 1MB } }
+	Write-host "#################################" -foreground yellow
+	if ($list) { $list } else { Write-host "No device found" -foreground red }
+
+}
+
+Function list_all_targets {
+	# Affichage des targets sans device
+	$list = $global:server.Targets | Format-Table -AutoSize -Wrap -Property Id, Name
+	Write-host "#################################" -foreground yellow
+	if ($list) { $list } else { Write-host "No target found" -foreground red }
+}
+
 
 # =======================================================
 # Fonction enumdevices
 # Utilisation : Affiche la liste des targets et des devices
 # =======================================================
 
-Function enumdevices
-{
+Function enumdevices {
 	Clear-host
-
-	# Affichage des targets et des devices rattachés
-	Write-host "#################################" -foreground yellow
-	Write-host "Targets and devices attached" -foreground yellow
-	$global:server.Devices | Where-Object TargetId -notlike "empty" | Format-Table -AutoSize -Wrap -Property TargetId, TargetName, DeviceId, @{N='Device Name';E={$_.Name}}, @{N='Path';E={$_.File}}, @{N='Size MB';E={$_.Size/1MB}}
-	
-	# Affichage des targets sans device
-	Write-host "#################################" -foreground yellow
-	Write-host "Targets wihout devices" -foreground yellow
-	$targetsToExclude = $global:server.Devices | Where-Object TargetId -notlike "empty" | Select-Object TargetId
-	$global:server.Targets | Where-Object Id -notin $targetsToExclude | Format-Table -AutoSize -Wrap -Property Id, Name
-	
-	# Affichage des devices attachés à aucun target
-	Write-host "#################################" -foreground yellow
-	Write-host "Devices not attached to a target" -foreground yellow
-	$global:server.Devices | Where-Object TargetId -like "empty" | Format-Table -AutoSize -Wrap  -Property DeviceId, @{N='Device Name';E={$_.Name}}, @{N='Path';E={$_.File}}, @{N='Size MB';E={$_.Size/1MB}}
-	
+	list_targets_and_devices_attached
+	list_targets_without_devices
+	list_orphan_devices
 	Read-host "Press enter to return to menu"
 }
 
@@ -137,11 +163,10 @@ Function enumdevices
 # Utilisation : Demande à l'utilisateur les informations pour la création d'un nouveau device et d'un nouveau target
 # =======================================================
 
-Function createdevice
-{
-    Clear-host
+Function createdevice {
+	Clear-host
 	Write-host "#################################" -foreground yellow
-    Write-host "Create image file and a new target"
+	Write-host "Create image file and a new target"
 	# Entrée des informations du device à créer
 	$filename = Read-Host "File Name"
 	$path = Read-Host "Path (exemple C:\Starwind\Storage)"
@@ -156,11 +181,9 @@ Function createdevice
 	Read-host "Press enter to return to menu"
 }
 
-Function removedevice
-{
+Function removedevice {
 	Clear-Host
-	Write-Host "#################################" -foreground yellow
-	$global:server.Devices | Where-Object TargetId -like "empty" | Format-Table -AutoSize -Wrap  -Property DeviceId, @{N='Device Name';E={$_.Name}}, @{N='Path';E={$_.File}}, @{N='Size MB';E={$_.Size/1MB}}
+	list_orphan_devices
 	write-host "#################################" -foreground yellow
 	$id = Read-Host "Enter the device's id to remove"
 	try {
@@ -169,64 +192,92 @@ Function removedevice
 		Remove-Item $devicefile
 		write-host "#################################" -foreground yellow
 		write-host "Device removed" -foreground yellow
-	} catch {write-host -Foreground Red $_}
+	}
+ catch { write-host -Foreground Red $_ }
 	$global:server.disconnect()
 	$global:server.connect()
 	Read-Host "Press enter to return to menu"
 }
 
-Function removetarget
-{
+# =======================================================
+# Fonction createtarget
+# Utilisation : Demande à l'utilisateur les informations pour la création d'un nouveau target
+# =======================================================
+
+Function createtarget {
+	Clear-host
+	Write-host "#################################" -foreground yellow
+	Write-host "Create a new target"
+	# Entrée des informations du device à créer
+	$alias = Read-Host "Target alias"
+	New-Target -server $global:server -alias $alias
+	Write-host "#################################" -foreground yellow
+	$global:server.disconnect()
+	$global:server.connect()
+	Read-host "Press enter to return to menu"
+}
+
+Function removetarget {
 	Clear-Host
-	Write-Host "#################################" -foreground yellow
-	$global:server.Targets | Format-Table -AutoSize -Wrap  -Property Id, Name
-	write-host "#################################" -foreground yellow
+	list_targets_without_devices
 	$name = Read-Host "Enter the target's name to remove"
 	try {
 		Remove-Target -server $global:server -name $name -force $true
 		write-host "#################################" -foreground yellow
 		write-host "Target removed" -foreground yellow
-	} catch {write-host -Foreground Red $_}
+	}
+ catch { write-host -Foreground Red $_ }
 	$global:server.disconnect()
 	$global:server.connect()
 	Read-Host "Press enter to return to menu"
 }
 
-Function extenddevice
-{
+Function extenddevice {
 	Clear-Host
-	Write-Host "#################################" -foreground yellow
-	foreach($device in $global:server.Devices)
-	{
-		$devicename = $device.name
-		$devicefile = $device.file
-		$devicesize = $device.size
-		Write-host "Name : $devicename"
-		Write-host "File : $devicefile"
-		Write-host "Size : $devicesize"
-		Write-host "-------------------" 
-	}
-
+	list_all_devices
 	write-host "#################################" -foreground yellow
 	$devicename = Read-Host "Device Name"
 	$size = Read-Host "Size in MB"
-
 	$device = Get-Device $global:server -name $deviceName
-	if( !$device )
-	{
-		Write-Host "Device not found" -foreground red
+	try {
+		$device.ExtendDevice($size)
+		$global:server.disconnect()
+		$global:server.connect()
+		Clear-Host
+		write-host "#################################" -foreground yellow
+		write-host "Device extended" -foreground yellow
+	} catch { write-host $_ }
+	Read-Host "Press enter to return to menu"
+}
+
+Function detach_attach {
+	Clear-Host
+	write-host "#################################" -foreground yellow
+	if ((Read-Host "Do you want to detach a device from a target ? [Y to execute]") -like "Y") {
+		list_targets_and_devices_attached
+		$targetId = Read-Host "Enter target id"
+		$deviceName = Read-Host "Enter Device name"
+		$target = $global:server.Targets | Where-Object Id -like $targetId
+		$target.detachdevice($deviceName)
+		$global:server.disconnect()
+		$global:server.connect()
+		write-host "Check result :" -foreground yellow
+		list_targets_and_devices_attached
 	}
-	else 
-	{
-		try {
-			$device.ExtendDevice($size) # Specify the amount of disk space you want to add to the virtual disk volume.
-			write-host "#################################" -foreground yellow
-			write-host "Image extended, check size :" -foreground yellow
-			$device = Get-Device $global:server -name $deviceName
-			$device
-		} catch {write-host $_}
-		Read-Host "Press enter to return to menu"
+	write-host "#################################" -foreground yellow
+	if ((Read-Host "Do you want to attach a device from a target ? [Y to execute]") -like "Y") {
+		list_targets_without_devices
+		list_orphan_devices
+		$targetId = Read-Host "Enter target id"
+		$deviceId = Read-Host "Enter Device id"
+		$target = $global:server.Targets | Where-Object Id -like $targetId
+		$target.attachdevice($deviceId)
+		$global:server.disconnect()
+		$global:server.connect()
+		write-host "Check result :" -foreground yellow
+		list_targets_and_devices_attached
 	}
+	Read-Host "Press enter to return to menu"
 }
 
 # =======================================================
